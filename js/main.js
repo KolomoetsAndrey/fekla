720927f39e01a90e0a8ba66bb3716490b91bedee
Andrey Kolomoets <kolomoets.and@gmail.com>
2021-12-12 23:37:13 +0200

    experts

--- js/main.js
+++ js/main.js
@@ -84,6 +84,45 @@ $(document).ready(function() {
         });
     }
 
+    //Expert Slider
+    if ($('.expert-service-slider').length) {
+        $('.expert-service-slider').slick({
+            dots: false,
+            infinite: false,
+            speed: 300,
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            infinite: true,
+            prevArrow: $('.expert-service-slider-prev'),
+            nextArrow: $('.expert-service-slider-next'),
+            responsive: [
+                {
+                    breakpoint: 1024,
+                    settings: {
+                        slidesToShow: 3,
+                        slidesToScroll: 1,
+                        infinite: true,
+                        dots: true
+                    }  
+                },
+                {
+                    breakpoint: 600,
+                    settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 1
+                    }
+                },
+                {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                    }
+                }
+            ]
+        });
+    }
+
     //Services Slider
     if ($('.services-slider-block').length) {
         $('.services-text-slider').slick({
@@ -194,6 +233,13 @@ $(document).ready(function() {
                     }
                 }
             ]
+        });
+    }
+
+    //Expert descr block height
+    if ($('.description-collapse').length) {
+        $('.description-collapse').click(function() {
+            $('.description-collapse').animate({ opacity: 0 });
         });
     }
 });
